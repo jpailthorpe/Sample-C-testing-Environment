0cad7a2c9e5e876d35ae59a4c772dc1e2b3457a0
Jpailthorpe <Jordanpailthorpe@gmail.com>
2016-04-05 19:14:59 -0400

    First pass
    Includes a spin and float script using while loops

---
+++ Sample C# testing Environment/Assets/Standard Assets/Characters/RollerBall/Scripts/Coin.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+
+	[SerializeField]
+	private float rotatespeed = 1.0f;
+
+	[SerializeField]
+	private float floatSpeed = 2.5f;
+
+	[SerializeField]
+	private float movementDistance = 100.5f;
+
+	private float startingY;
+	private bool isMovingUp = true;
+	
+	// Use this for initialization
+	void Start () {
+		startingY = transform.position.y;
+		transform.Rotate (transform.up, Random.Range (0f, 360f));
+		StartCoroutine (Spin ());
+		StartCoroutine (Float ());
+
+	}
+
+	private IEnumerator Spin()
+	{
+		while (true) {
+			transform.Rotate (transform.up, 360 * rotatespeed * Time.deltaTime);
+			yield return 0;
+		}
+	}
+
+	private IEnumerator Float()
+	{
+		while (true) {
+			float newY = transform.position.y + (isMovingUp ? 1 : -1) * movementDistance * floatSpeed * Time.deltaTime;
+			if (newY > startingY + movementDistance)
+			{
+				newY = startingY + movementDistance;
+				isMovingUp = false;
+			}
+			else if (newY < startingY)
+			{
+				newY = startingY;
+				isMovingUp = true;
+			}
+			transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+			yield return 0;
+
+		}
+	}
+}
---
+++ Sample C# testing Environment/Assets/Standard Assets/Vehicles/Aircraft/Scripts/MyCoolClass.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCoolClass : MonoBehaviour {
+
+	public string hello;
+	public bool Abool;
+	public GameObject Aobject;
+
+	[SerializeField]
+	private int Aint;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+}
